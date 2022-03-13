c3bcfdea7c7a8547e92119ea682ad37ea11baeae
racesaunders <race.saunders@gmail.com>
2022-03-13 14:03:50 -0400

    lab6complete

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,9 +1,11 @@
 function getRandomIntInclusive(min, max) {
-  return max;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 // As the last step of your lab, hook this up to index.html
-function restoArrayMake(dataArray) {
+function dataHandler(dataArray) {
   console.log('fired datahandler');
   console.table(dataArray);
   const range = [...Array(15).keys()];
@@ -36,10 +38,11 @@ async function mainEvent() {
   submit.style.display = 'none';
 
   const results = await fetch(
-    'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'
+    '/api/foodServicesPG'
   ); // This accesses some data from our API
   const arrayFromJson = await results.json(); // This changes it into data we can use - an object
-  console.table(arrayFromJson); // this is called "dot notation"
+ 
+  // console.table(arrayFromJson.data); // this is called "dot notation"
   // arrayFromJson.data - we're accessing a key called 'data' on the returned object
   // it contains all 1,000 records we need
   if (arrayFromJson.data.length > 0) {
@@ -48,7 +51,7 @@ async function mainEvent() {
       // async has to be declared all the way to get an await
       submitEvent.preventDefault(); // This prevents your page from refreshing!
       console.log('form submission'); // this is substituting for a "breakpoint"
-      const restoArray = restoArrayMake(arrayFromJson.data);
+      const restoArray = dataHandler(arrayFromJson.data);
       createHtmlList(restoArray);
     });
   }
