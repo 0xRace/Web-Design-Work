fcbb096cc252575b617b01092b0bf1be30c6e851
racesaunders <race.saunders@gmail.com>
2022-04-01 20:26:53 -0400

    flappybirdcomplete

--- client/flappy/script.js
+++ client/flappy/script.js
@@ -1,175 +1,81 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const grid = document.querySelector('.grid');
-  const doodler = document.createElement('div');
-  let doodlerLeftSpace = 50;
-  let startPoint = 150;
-  let doodlerBottomSpace = startPoint;
+  const bird = document.querySelector('.bird');
+  const gameDisplay = document.querySelector('.game-container');
+  const ground = document.querySelector('.ground');
+
+  let birdLeft = 220;
+  let birdBottom = 100;
+  let gravity = 2;
   let isGameOver = false;
-  const platformCount = 5;
-  const platforms = [];
-  let upTimerId;
-  let downTimerId;
-  let isJumping = true;
-  let isGoingLeft = false;
-  let isGoingRight = false;
-  let leftTimerId;
-  let rightTimerId;
-  let score = 0;
+  let gap = 430;
 
-  function createDoodler() {
-    grid.appendChild(doodler);
-    doodler.classList.add('doodler');
-    doodlerLeftSpace = platforms[0].left;
-    doodler.style.left = `${doodlerLeftSpace}px`;
-    doodler.style.bottom = `${doodlerBottomSpace}px`;
+  function startGame() {
+    birdBottom -= gravity;
+    bird.style.bottom = birdBottom + 'px';
+    bird.style.left = birdLeft + 'px';
   }
+  let gameTimerId = setInterval(startGame, 20);
 
-  class Platform {
-    constructor(newPlatBottom) {
-      this.bottom = newPlatBottom;
-      this.left = Math.random() * 315;
-      this.visual = document.createElement('div');
-
-      const {visual} = this;
-      visual.classList.add('platform');
-      visual.style.left = `${this.left}px`;
-      visual.style.bottom = `${this.bottom}px`;
-      grid.appendChild(visual);
+  function control(e) {
+    if (e.keyCode === 32) {
+      jump();
     }
-  }
-  function createPlatforms() {
-    for (let i = 0; i < 5; i += 1) {
-      const platGap = 600 / platformCount;
-      const newPlatBottom = 100 + i * platGap;
-      const newPlatform = new Platform(newPlatBottom);
-      platforms.push(newPlatform);
-      console.log(platforms);
-    }
-  }
-
-  function movePlatforms() {
-    if (doodlerBottomSpace > 200) {
-      platforms.forEach((platform) => {
-        platform.bottom -= 4;
-        const {visual} = platform;
-        visual.style.bottom = `${platform.bottom}px`;
-
-        if (platform.bottom < 10) {
-          let firstPlatform = platforms[0].visual;
-          firstPlatform.classList.remove('platform');
-          platforms.shift();
-          score += 1;
-          console.log(platforms);
-          let newPlatform = new Platform(600);
-          platforms.push(newPlatform);
-        }
-      });
-    }
-  }
-  function gameOver() {
-    console.log('game over');
-    isGameOver = true;
-    while (grid.firstChild) {
-      grid.removeChild(grid.firstChild);
-    }
-    grid.innerHTML = score;
-    clearInterval(upTimerId);
-    clearInterval(downTimerId);
-    clearInterval(leftTimerId);
-    clearInterval(rightTimerId);
-  }
-
-  function fall() {
-    clearInterval(upTimerId);
-    isJumping = false;
-    downTimerId = setInterval(() => {
-      doodlerBottomSpace -= 5;
-      doodler.style.bottom = `${doodlerBottomSpace}px`;
-      if (doodlerBottomSpace <= 0) {
-        gameOver();
-      }
-      platforms.forEach((platform) => {
-        if (
-          (doodlerBottomSpace >= platform.bottom)
-          && (doodlerBottomSpace <= platform.bottom + 15)
-          && ((doodlerLeftSpace + 60) >= platform.left)
-          && (doodlerLeftSpace <= (platform.left + 85))
-          && !isJumping
-        ) {
-          console.log('landed');
-          startPoint = doodlerBottomSpace;
-          jump();
-        }
-      });
-    }, 30);
   }
   function jump() {
-    clearInterval(downTimerId);
-    isJumping = true;
-    upTimerId = setInterval(() => {
-      doodlerBottomSpace += 20;
-      doodler.style.bottom = `${doodlerBottomSpace}px`;
-      if (doodlerBottomSpace > startPoint + 200) {
-        fall();
-      }
-    }, 30);
+    if (birdBottom < 500) birdBottom += 50;
+    bird.style.bottom = birdBottom + 'px';
+    console.log(birdBottom);
   }
-  function control(e) {
-    if (e.key === 'ArrowLeft') {
-      // move left
-      moveLeft();
-    } else if (e.key === 'ArrowRight') {
-      // move right
-      moveRight();
-    } else if (e.key === 'ArrowUp') {
-      // move straight
-      moveStraight();
-    }
-  }
-  function moveLeft() {
-    if (isGoingRight) {
-      clearInterval(rightTimerId);
-      isGoingRight = false;
-    }
-    isGoingLeft = true;
-    leftTimerId = setInterval(() => {
-      if (doodlerLeftSpace >= 0) {
-        doodlerLeftSpace -= 5;
-        doodler.style.left = `${doodlerLeftSpace}px`;
-      } else moveRight();
-    }, 20);
-  }
+  document.addEventListener('keyup', control);
 
-  function moveRight() {
-    if (isGoingLeft) {
-      clearInterval(leftTimerId);
-      isGoingLeft = false;
-    }
-    isGoingRight = true;
-    rightTimerId = setInterval(() => {
-      if (doodlerLeftSpace <= 340) {
-        doodlerLeftSpace += 5;
-        doodler.style.left = `${doodlerLeftSpace}px`;
-      } else moveLeft();
-    }, 20);
-  }
-  function moveStraight() {
-    isGoingRight = false;
-    isGoingLeft = false;
-    clearInterval(rightTimerId);
-    clearInterval(leftTimerId);
-  }
+  function generateObstacle() {
+    let obstacleLeft = 500;
+    let randomHeight = Math.random() * 60;
+    let obstacleBottom = randomHeight;
+    const obstacle = document.createElement('div');
+    const topObstacle = document.createElement('div');
 
-  function start() {
+
     if (!isGameOver) {
-      createPlatforms();
-      createDoodler();
-      setInterval(movePlatforms, 30);
-      jump();
-      document.addEventListener('keyup', control);
+      obstacle.classList.add('obstacle');
+      topObstacle.classList.add('topObstacle');
     }
-  }
+    gameDisplay.appendChild(obstacle);
+    gameDisplay.appendChild(topObstacle);
+    obstacle.style.left = obstacleLeft + 'px';
+    topObstacle.style.left = obstacleLeft + 'px';
+    obstacle.style.bottom = obstacleBottom + 'px';
+    topObstacle.style.bottom = obstacleBottom + gap + 'px';
 
-  // attach to buttom
-  start();
+    function moveObstacle() {
+      obstacleLeft -= 2;
+      obstacle.style.left = obstacleLeft + 'px';
+      topObstacle.style.left = obstacleLeft + 'px';
+
+
+      if (obstacleLeft === -60) {
+        clearInterval(timerId);
+        gameDisplay.removeChild(obstacle);
+        gameDisplay.removeChild(topObstacle);
+      }
+      if (obstacleLeft > 200
+        && obstacleLeft < 280
+        && birdLeft === 220
+        && (birdBottom < obstacleBottom + 153 || birdBottom > obstacleBottom + gap - 200)
+        || birdBottom === 0) {
+        gameOver();
+        clearInterval(timerId);
+      }
+    }
+    let timerId = setInterval(moveObstacle, 20);
+    if (!isGameOver) setTimeout(generateObstacle, 3000);
+  }
+  generateObstacle();
+
+  function gameOver() {
+    clearInterval(gameTimerId);
+    console.log('game over');
+    isGameOver = true;
+    document.removeEventListener('keyup', control);
+  }
 });
